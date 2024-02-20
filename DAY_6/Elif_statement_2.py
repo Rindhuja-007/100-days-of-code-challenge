e0f1a08b0aa7589625dff1ba03e1d071677fc412
Rindhuja chandramohan <145462351+Rindhuja-007@users.noreply.github.com>
2024-02-20 22:16:38 +0530

    Create Elif_statement_2.py

---
+++ DAY_6/Elif_statement_2.py
@@ -0,0 +1,10 @@
+#simple login page
+print("login_system")
+username=input("username: ")
+password=input("password: ")
+if username=="rindhu" and password=="2004":
+    print("hey,rindhu-long time no see")
+elif username=="angel" and password=="2003":
+    print("hey,angel-how was your palcement training? i hope you are doing well")
+else:
+    print("i don't know who you are..")
