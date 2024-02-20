dca32a2790ddf6e8da75a7fd9e34435e54691195
Rindhuja chandramohan <145462351+Rindhuja-007@users.noreply.github.com>
2024-02-20 22:06:22 +0530

    Create Elif_Staement.py

---
+++ DAY_6/Elif_Staement.py
@@ -0,0 +1,12 @@
+print("secured login")
+username=input("username: ")
+password=input("password: ")
+if username=="rindhu" and password=="2004":
+  print("hey,rindhu")
+elif username=="angel" and password=="2003":
+  print("hello,angel")
+elif username=="naruto" and password=="2009":
+  print("welcome,dude")
+else:
+  print("who the hell are you?")
+  
