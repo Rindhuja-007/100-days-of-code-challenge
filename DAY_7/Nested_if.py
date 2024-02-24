1625e03bfd830071ec388fd7c3769860bf97e073
Rindhuja chandramohan <145462351+Rindhuja-007@users.noreply.github.com>
2024-02-24 18:51:07 +0530

    Create Nested_if.py

---
+++ DAY_7/Nested_if.py
@@ -0,0 +1,18 @@
+print("did you watch anime ?")
+ans=input("ans: ")
+if ans=="yes":
+    print("nice!,what's your favorite anime ?")
+    ans2=input("fav anime is ")
+    if ans2=="naruto":
+        print("that's awesome")
+        print("who's ur fav charecter? ")
+        ans3=input("fav charecter is ")
+        if ans3=="obito":
+            print("i too love him😍")
+        else:
+            print("ohh good")
+    else:
+        print("ohh,that's cool😊")
+else:
+    print("you should watch one.")
+          
